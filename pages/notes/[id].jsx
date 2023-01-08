19b7992c9a86fd645096b47beb2aa3db4266302f
Karun Kamal <karundeveloper@gmail.com>
2023-01-08 20:09:30 +0530

    dynamic route with param

---
+++ pages/notes/[id].jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+
+export default function Note() {
+
+
+    return (<div>
+        <Head>
+            <title>Note: </title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <p>This is a specific note - with note id: </p>
+    </div>)
+}
